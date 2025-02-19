2c9b25b8e1b54e5807018c55dc6f4b967ab3ff7a
Bryan Lustri <bryandiegolustri2008@gmail.com>
2025-02-19 10:33:55 -0300

    Aula de introdução ao PHP

---
+++ aula01/olamundo.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <style>
+        .texto {
+            color: '#000F'
+ 
+        }
+     </style>
+    
+        <body>
+     <p>
+
+        <?php
+            echo "My first PHP script!";
+        ?>
+     </p>
+
+    </body>
+</html>
